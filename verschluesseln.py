faeacf66cf3e16c85e9a67f125fabaefea30d66c
Nils897 <fleschhut.nils@gmail.com>
2024-08-27 10:24:13 +0200

    Update verschluesseln.py

--- verschluesseln.py
+++ verschluesseln.py
@@ -10,7 +10,6 @@ The module includes functions to:
 """
 import json
 import os
-import cryptography
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
@@ -28,7 +27,6 @@ def encrypt_data(data, key, iv):
     """
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(data) + padder.finalize()
-
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
@@ -46,7 +44,6 @@ def decrypt_data(encrypted_data, key, iv):
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     padded_data = decryptor.update(encrypted_data) + decryptor.finalize()
-
     unpadder = padding.PKCS7(128).unpadder()
     data = unpadder.update(padded_data) + unpadder.finalize()
     return data
@@ -64,7 +61,6 @@ def save_encrypted_dict_to_file(data_dict, output_filename, password):
     """
     json_string = json.dumps(data_dict)
     data = json_string.encode('utf-8')
-
     salt = os.urandom(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -74,10 +70,8 @@ def save_encrypted_dict_to_file(data_dict, output_filename, password):
         backend=default_backend()
     )
     key = kdf.derive(password.encode())
-
     iv = os.urandom(16)
     encrypted_data = encrypt_data(data, key, iv)
-
     with open(output_filename, 'wb') as file:
         file.write(salt + iv + encrypted_data)
 
@@ -96,7 +90,6 @@ def load_encrypted_dict_from_file(input_filename, password):
         salt = file.read(16)
         iv = file.read(16)
         encrypted_data = file.read()
-
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -105,7 +98,6 @@ def load_encrypted_dict_from_file(input_filename, password):
         backend=default_backend()
     )
     key = kdf.derive(password.encode())
-
     decrypted_data = decrypt_data(encrypted_data, key, iv)
     json_string = decrypted_data.decode('utf-8')
     return json.loads(json_string)
