77b082199a229956f84632cb14df7eb850545cac
Nils897 <fleschhut.nils@gmail.com>
2024-08-27 10:23:28 +0200

    rename

--- verschluesseln.py
+++ verschluesseln.py
@@ -1,3 +1,13 @@
+"""
+This module provides functionality for encrypting and decrypting data, 
+specifically dictionaries, using AES encryption with a password-derived key.
+
+The module includes functions to:
+- Encrypt data using AES encryption in CFB mode.
+- Decrypt data that was encrypted with AES in CFB mode.
+- Save a Python dictionary to a file as encrypted data.
+- Load and decrypt a Python dictionary from an encrypted file.
+"""
 import json
 import os
 import cryptography
@@ -8,6 +18,14 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def encrypt_data(data, key, iv):
+    """
+    Encrypts the given data using AES encryption in CFB mode with the specified key and initialization vector (IV).
+
+    :param data: The plaintext data to be encrypted (in bytes).
+    :param key: The encryption key (in bytes).
+    :param iv: The initialization vector (IV) for the cipher (in bytes).
+    :return: The encrypted data (in bytes).
+    """
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(data) + padder.finalize()
 
@@ -17,6 +35,14 @@ def encrypt_data(data, key, iv):
     return encrypted_data
 
 def decrypt_data(encrypted_data, key, iv):
+    """
+    Decrypts the given encrypted data using AES decryption in CFB mode with the specified key and initialization vector (IV).
+
+    :param encrypted_data: The data to be decrypted (in bytes).
+    :param key: The decryption key (in bytes).
+    :param iv: The initialization vector (IV) used during encryption (in bytes).
+    :return: The decrypted plaintext data (in bytes).
+    """
     cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     padded_data = decryptor.update(encrypted_data) + decryptor.finalize()
@@ -26,6 +52,16 @@ def decrypt_data(encrypted_data, key, iv):
     return data
 
 def save_encrypted_dict_to_file(data_dict, output_filename, password):
+    """
+    Encrypts a dictionary and saves it to a file using a password-derived key.
+
+    The dictionary is first serialized into a JSON string and then encrypted using AES encryption.
+    The encryption key is derived from the provided password using PBKDF2 with a random salt.
+
+    :param data_dict: The dictionary to be encrypted and saved.
+    :param output_filename: The name of the file to save the encrypted data to.
+    :param password: The password used to derive the encryption key.
+    """
     json_string = json.dumps(data_dict)
     data = json_string.encode('utf-8')
 
@@ -46,6 +82,16 @@ def save_encrypted_dict_to_file(data_dict, output_filename, password):
         file.write(salt + iv + encrypted_data)
 
 def load_encrypted_dict_from_file(input_filename, password):
+    """
+    Loads and decrypts an encrypted dictionary from a file using a password-derived key.
+
+    The function reads the salt, IV, and encrypted data from the file, derives the decryption key 
+    using the provided password, and decrypts the data back into its original dictionary form.
+
+    :param input_filename: The name of the file containing the encrypted data.
+    :param password: The password used to derive the decryption key.
+    :return: The decrypted dictionary.
+    """
     with open(input_filename, 'rb') as file:
         salt = file.read(16)
         iv = file.read(16)
