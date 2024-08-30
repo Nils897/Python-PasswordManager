89f2730d40230622ca62c9b3c16ddbea56b4f438
Nils897 <fleschhut.nils@gmail.com>
2024-08-30 14:54:26 +0200

    Update test_password_validation.py

--- tests/test_password_validation.py
+++ tests/test_password_validation.py
@@ -1,3 +1,4 @@
+# pylint: disable=C
 import unittest
 import requests
 from unittest.mock import patch
@@ -26,22 +27,27 @@ class TestPasswordValidation(unittest.TestCase):
     @patch('source.password_validation.is_password_pwned', return_value=False)
     def test_is_password_correct_not_pwned(self, mock_pwned):
         self.assertTrue(is_password_correct("Secure1@Password"))
+        mock_pwned.assert_called_once()
 
     @patch('source.password_validation.is_password_pwned', return_value=True)
     def test_is_password_correct_pwned(self, mock_pwned):
         self.assertFalse(is_password_correct("Pwned1@Password"))
+        mock_pwned.assert_called_once()
 
     @patch('source.password_validation.request_api', return_value="1E4C9B93F3F0682250B6CF8331B7EE68FD8:2")
     def test_is_password_pwned_true(self, mock_request_api):
         self.assertTrue(is_password_pwned("password"))
+        mock_request_api.assert_called_once()
 
     @patch('source.password_validation.request_api', return_value="")
     def test_is_password_pwned_false(self, mock_request_api):
         self.assertFalse(is_password_pwned("password"))
+        mock_request_api.assert_called_once()
 
     @patch('source.password_validation.request_api', return_value=None)
     def test_is_password_pwned_api_failure(self, mock_request_api):
         self.assertFalse(is_password_pwned("password"))
+        mock_request_api.assert_called_once()
 
     @patch('source.password_validation.requests.get')
     def test_request_api_success(self, mock_get):
